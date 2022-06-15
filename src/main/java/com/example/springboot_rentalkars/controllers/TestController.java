29a44f9e6f00c4de1bdeff6fbe64fe861f3154a4
Andrea-LaRoss <andrea.199020@gmail.com>
2022-06-15 18:57:39 +0200

    TestCommit

--- src/main/java/com/example/springboot_rentalkars/controllers/TestController.java
+++ src/main/java/com/example/springboot_rentalkars/controllers/TestController.java
@@ -1,4 +1,15 @@
 package com.example.springboot_rentalkars.controllers;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/test")
 public class TestController {
+
+    @GetMapping()
+    public String getTest() {
+        return "The cake is a lie";
+    }
 }
