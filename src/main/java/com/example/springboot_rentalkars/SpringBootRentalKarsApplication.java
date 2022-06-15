06083f34c962d4b14bf0336c76f2904ecd272f77
Andrea-LaRoss <andrea.199020@gmail.com>
2022-06-15 18:10:06 +0200

    TestCommit

---
+++ src/main/java/com/example/springboot_rentalkars/SpringBootRentalKarsApplication.java
@@ -0,0 +1,13 @@
+package com.example.springboot_rentalkars;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRentalKarsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootRentalKarsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springboot_rentalkars/SpringBootRentalKarsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springboot_rentalkars;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootRentalKarsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
