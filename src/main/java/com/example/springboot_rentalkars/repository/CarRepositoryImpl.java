782c336c8b3cc0af9d00fe8144e50dc82841ebe5
Andrea-LaRoss <andrea.199020@gmail.com>
2022-06-20 15:51:48 +0200

    DTO

--- src/main/java/com/example/springboot_rentalkars/repository/CarRepositoryImpl.java
+++ src/main/java/com/example/springboot_rentalkars/repository/CarRepositoryImpl.java
@@ -29,4 +29,153 @@ public class CarRepositoryImpl implements CarRepository{
         return null;
     }
 
+    @Override
+    public List<Car> findAll() {
+        return null;
+    }
+
+    @Override
+    public List<Car> findAll(Sort sort) {
+        return null;
+    }
+
+    @Override
+    public Page<Car> findAll(Pageable pageable) {
+        return null;
+    }
+
+    @Override
+    public List<Car> findAllById(Iterable<Long> longs) {
+        return null;
+    }
+
+    @Override
+    public long count() {
+        return 0;
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+
+    }
+
+    @Override
+    public void delete(Car entity) {
+
+    }
+
+    @Override
+    public void deleteAllById(Iterable<? extends Long> longs) {
+
+    }
+
+    @Override
+    public void deleteAll(Iterable<? extends Car> entities) {
+
+    }
+
+    @Override
+    public void deleteAll() {
+
+    }
+
+    @Override
+    public <S extends Car> S save(S entity) {
+        return null;
+    }
+
+    @Override
+    public <S extends Car> List<S> saveAll(Iterable<S> entities) {
+        return null;
+    }
+
+    @Override
+    public Optional<Car> findById(Long aLong) {
+        return Optional.empty();
+    }
+
+    @Override
+    public boolean existsById(Long aLong) {
+        return false;
+    }
+
+    @Override
+    public void flush() {
+
+    }
+
+    @Override
+    public <S extends Car> S saveAndFlush(S entity) {
+        return null;
+    }
+
+    @Override
+    public <S extends Car> List<S> saveAllAndFlush(Iterable<S> entities) {
+        return null;
+    }
+
+    @Override
+    public void deleteAllInBatch(Iterable<Car> entities) {
+
+    }
+
+    @Override
+    public void deleteAllByIdInBatch(Iterable<Long> longs) {
+
+    }
+
+    @Override
+    public void deleteAllInBatch() {
+
+    }
+
+    @Override
+    public Car getOne(Long aLong) {
+        return null;
+    }
+
+    @Override
+    public Car getById(Long aLong) {
+        return null;
+    }
+
+    @Override
+    public Car getReferenceById(Long aLong) {
+        return null;
+    }
+
+    @Override
+    public <S extends Car> Optional<S> findOne(Example<S> example) {
+        return Optional.empty();
+    }
+
+    @Override
+    public <S extends Car> List<S> findAll(Example<S> example) {
+        return null;
+    }
+
+    @Override
+    public <S extends Car> List<S> findAll(Example<S> example, Sort sort) {
+        return null;
+    }
+
+    @Override
+    public <S extends Car> Page<S> findAll(Example<S> example, Pageable pageable) {
+        return null;
+    }
+
+    @Override
+    public <S extends Car> long count(Example<S> example) {
+        return this.count();
+    }
+
+    @Override
+    public <S extends Car> boolean exists(Example<S> example) {
+        return this.exists(example);
+    }
+
+    @Override
+    public <S extends Car, R> R findBy(Example<S> example, Function<FluentQuery.FetchableFluentQuery<S>, R> queryFunction) {
+        return findBy(example);
+    }
 }
